9a550176b98cd00d1aa2657b5d16779814e3ea1c
xiezl <xiezhongle@comp.nus.edu.sg>
2020-04-12 13:31:54 +0800

    cool-website#2 Fix building bugs and typos.
    TODO: Make the video responsive accordingly.

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,7 @@ module.exports = {
   publicPath: './',
   outputDir: 'dist',
   devServer: {
+    // TODO: Change the comments to English.
     open: true, // 是否自动弹出浏览器页面
     https: false, // 是否使用https协议
     hotOnly: true, // 是否开启热更新

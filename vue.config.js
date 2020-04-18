c121744d715b60bf074c8ede68cc2d5029b37991
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-18 02:06:51 +0000

    Deploy cool-squad/cool-website to github.com/cool-squad/cool-website.git:gh-pages

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ function resolve(dir) {
 
 module.exports = {
   // publicPath: './',
-  publicPath: process.env.NODE_ENV === 'production' ? '/cool-website' : './',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : './',
   outputDir: process.env.NODE_ENV === 'production' ? 'doc' : 'dist',
   // outputDir: 'dist',
   devServer: {
